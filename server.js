0f61bee344857fcf727181d2ce70d94b6163e99b
Miguel Angel Cavazos Reyes <miguel.cavazos@ad.infosys.com>
2020-06-21 14:33:31 -0500

    Required GraphQL schema

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 // Requiring express dependency
 const express = require('express');
 
+// Requiring the schema to use in the request handler express-graphql
+const schema = require('./schema.js');
+
+// Requiring express-graphql dependency
+const expressGraphQL = require('express-graphql');
+
 // Creating an instance of express
 const app = express();
+
+// Creating the entry point and using the request handler to use the GraphQL standard
+app.use('/graphql', expressGraphQL({
+	schema: schema,
+	graphiql: true
+}));
 
 // Accessing the express instance to create a server to access to port 4000
 app.listen(4000, () => {
