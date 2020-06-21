01fa17f3cf9e9d55c5a9f23265d8f1b1418a367d
Miguel Angel Cavazos Reyes <miguel.cavazos@ad.infosys.com>
2020-06-21 14:34:12 -0500

    Created the schema, required all the dependencies needed to create a schema, exported the schema

---
+++ schema.js
@@ -0,0 +1,14 @@
+// Requiring all the needed dependencies from graphql to create a schema
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
+	GraphQLList,
+	GraphQLNonNull
+} = require('graphql');
+
+// Exporting the schema
+module.exports = new GraphQLSchema({
+
+});
